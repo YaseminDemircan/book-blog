b3154d22bdfb4f897c994084f59c0799bd053eda
Yasemin Demircan <demircan.yasemin@hotmail.com>
2018-09-04 14:03:57 +0300

    book blog

---
+++ BookBlogMVC/Controllers/AdminController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookBlogMVC.Models;
+using System.Drawing;
+
+namespace BookBlogMVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        BlogMVCdbEntities db = new BlogMVCdbEntities();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            var makaleler = db.Articles.ToList();
+            return View(makaleler);
+        }
+
+        // GET: Admin/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Admin/Create
+        public ActionResult Create()
+        {
+            ViewBag.KategoriID = new SelectList(db.Kategoris, "KategoriId", "Adi");
+            ViewBag.YazarID = new SelectList(db.Yazars, "YazarId", "Adi");
+            ViewBag.YazarID = new SelectList(db.Yazars, "YazarId", "Soyadi");
+            return View();
+        }
+
+        // POST: Admin/Create
+        [HttpPost]
+        public ActionResult Create(Article makale,  HttpPostedFileBase resim)
+        {
+            try
+            {
+                Image img = Image.FromStream(resim.InputStream);
+                Bitmap Resim = new Bitmap(img);
+                Resim.Save(Server.MapPath("/Images/" + resim.FileName));
+
+                Resim rsm = new Resim();
+                rsm.BuyukBoyut = "/Images/" + resim.FileName;
+                
+                db.Resims.Add(rsm);
+                db.SaveChanges();
+
+                makale.ResimID = rsm.ResimId;
+                makale.GoruntulenmeSayisi = 0;
+                makale.Begeni = 0;
+                db.Articles.Add(makale);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var makale = db.Articles.Where(x => x.MakaleId == id).SingleOrDefault();
+            if (makale == null)
+            {
+                return HttpNotFound();
+            }
+
+            ViewBag.KategoriID = new SelectList(db.Kategoris, "KategoriId", "Adi", makale.KategoriID);
+            ViewBag.YazarID = new SelectList(db.Yazars, "YazarId", "Adi", makale.YazarID);
+            return View(makale);
+        }
+
+        // POST: Admin/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Article mkl, HttpPostedFileBase resim)
+        {
+            try
+            {
+                var makale = db.Articles.Where(x => x.MakaleId == id).SingleOrDefault();
+                if (resim != null)
+                {
+                    if (System.IO.File.Exists(Server.MapPath(makale.Resim.BuyukBoyut)))
+                    {
+                        System.IO.File.Delete(Server.MapPath(makale.Resim.BuyukBoyut));
+                    }
+                    Image img = Image.FromStream(resim.InputStream);
+                    Bitmap Resim = new Bitmap(img);
+                    Resim.Save(Server.MapPath("/Images/" + resim.FileName));
+
+                    Resim rsm = new Resim();
+                    rsm.BuyukBoyut = "/Images/" + resim.FileName;
+
+                    db.Resims.Add(rsm);
+                    db.SaveChanges();
+
+                    makale.ResimID = rsm.ResimId;
+                }
+                makale.Baslik = mkl.Baslik;
+                makale.Gelisme = mkl.Gelisme;
+                makale.Giris = mkl.Giris;
+                makale.Sonuc = mkl.Sonuc;
+                makale.KategoriID = mkl.KategoriID;
+                makale.EklenmeTarihi = mkl.EklenmeTarihi;
+                makale.GoruntulenmeSayisi = mkl.GoruntulenmeSayisi;
+                makale.Begeni = mkl.Begeni;
+                makale.YazarID = mkl.YazarID;
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var makale = db.Articles.Where(x => x.MakaleId == id).SingleOrDefault();
+            if (makale == null)
+            {
+                return HttpNotFound();
+            }
+            return View(makale);
+        }
+
+        // POST: Admin/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                var makale = db.Articles.Where(x => x.MakaleId == id).SingleOrDefault();
+                if (makale == null)
+                {
+                    return HttpNotFound();
+                }
+                if (System.IO.File.Exists(Server.MapPath(makale.Resim.BuyukBoyut)))
+                {
+                    System.IO.File.Delete(Server.MapPath(makale.Resim.BuyukBoyut));
+                }
+                foreach (var y in makale.Yorums.ToList())
+                {
+                    db.Yorums.Remove(y);
+                }
+                db.Articles.Remove(makale);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BookBlogMVC/Controllers/HomeController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookBlogMVC.Models;
+using PagedList;
+using PagedList.Mvc;
+
+namespace BookBlogMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        BlogMVCdbEntities db = new BlogMVCdbEntities();
+
+        public ActionResult Index(int? page)
+        {
+            var makale = db.Articles.ToList().ToPagedList(page ?? 1,5);
+            return View(makale);
+        }
+
+        public PartialViewResult Kategori()
+        {
+            var kategoriler = db.Kategoris.ToList();
+            return PartialView(kategoriler);
+        }
+
+        public PartialViewResult CokOkunanlar()
+        {
+            var populer = db.Articles.OrderBy(x => x.EklenmeTarihi).ToList();
+            return PartialView(populer);
+        }
+
+        public ActionResult MakaleDetay(int id)
+        {
+            var makale = db.Articles.Where(x => x.MakaleId == id).SingleOrDefault();
+            if (makale == null)
+            {
+                return HttpNotFound();
+            }
+            return View(makale);
+        }
+
+        public ActionResult KategoriMakale(int id, int? page)
+        {
+            ViewBag.PageTitle = db.Kategoris.Where(x => x.KategoriId == id).Select(x=>x.Adi).SingleOrDefault();
+            ViewBag.Aciklama = db.Kategoris.Where(x => x.KategoriId == id).Select(x => x.Aciklama).SingleOrDefault();
+            var kategori = db.Articles.Where(x => x.KategoriID == id).ToList().ToPagedList(page ?? 1, 4);
+            return View(kategori);
+        }
+
+        public ActionResult YazarMakale(int id, int? page)
+        {
+            ViewBag.PageTitleAd = db.Yazars.Where(x => x.YazarId == id).Select(x => x.Adi).SingleOrDefault();
+            ViewBag.PageTitleSoyad = db.Yazars.Where(x => x.YazarId == id).Select(x => x.Soyadi).SingleOrDefault();
+            var yazar = db.Articles.Where(x => x.YazarID == id).ToList().ToPagedList(page ?? 1, 4);
+            return View(yazar);
+        }
+
+        public ActionResult Ara(int? page, string Ara = null)
+        {   
+            var aranan = db.Articles.Where(x => (x.Giris.Contains(Ara)) || (x.Baslik.Contains(Ara)) || (x.Gelisme.Contains(Ara)) || (x.Sonuc.Contains(Ara))).ToList().ToPagedList(page ?? 1, 4);
+            return View(aranan);
+        }
+
+        public JsonResult YorumYap(string yorum, int Makaleid)
+        {
+            if (yorum != null)
+            {
+                db.Yorums.Add(new Yorum { MakaleID = Makaleid, Yorum1 = yorum, EklenmeTarihi = DateTime.Now });
+                db.SaveChanges();
+            }
+            return Json(false, JsonRequestBehavior.AllowGet);
+        }
+
+        
+        public string Begen(int id)
+        {
+            Article mkl = db.Articles.FirstOrDefault(x => x.MakaleId == id);
+            mkl.Begeni++;
+            db.SaveChanges();
+            return mkl.Begeni.ToString();
+        }
+
+        public string Goruntuleme(int Makaleid)
+        {
+            var mkl = db.Articles.Where(x => x.MakaleId == Makaleid).FirstOrDefault();
+            mkl.GoruntulenmeSayisi++;
+            db.SaveChanges();
+            return mkl.GoruntulenmeSayisi.ToString();
+        }
+    }
+}
